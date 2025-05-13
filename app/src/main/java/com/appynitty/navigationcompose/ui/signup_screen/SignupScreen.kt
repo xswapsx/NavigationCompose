d9b38ab66e9fdf42d23821510d589d7a48706d95
Swapnil Lanjewar <xswapsx@gmail.com>
2025-05-13 11:08:58 +0530

    Feat: Added validation and toast message in Signup Screen
    - Added basic validation to check if all fields are filled in the Signup Screen.
    - Displayed a Toast message if any required field is empty when the signup button is clicked.
    - Introduced `LocalContext` to display the Toast message.

--- app/src/main/java/com/appynitty/navigationcompose/ui/signup_screen/SignupScreen.kt
+++ app/src/main/java/com/appynitty/navigationcompose/ui/signup_screen/SignupScreen.kt
@@ -1,5 +1,6 @@
 package com.appynitty.navigationcompose.ui.signup_screen
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -29,6 +30,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -48,6 +50,8 @@ fun SignupScreen(
     var phone by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var isPasswordVisible by remember { mutableStateOf(false) }
+
+    val context = LocalContext.current
 
     Scaffold(
         modifier = modifier.fillMaxSize(),
@@ -156,7 +160,10 @@ fun SignupScreen(
                     // Placeholder for signup action
                     if (name.isNotBlank() && email.isNotBlank() && phone.isNotBlank() && password.isNotBlank()) {
                         // Example: Navigate to a dashboard screen (if exists)
-                         navActions.navigateToMainGraph()
+                        navActions.navigateToMainGraph()
+                    } else {
+                        Toast.makeText(context, "All fields are mandatory!", Toast.LENGTH_SHORT)
+                            .show()
                     }
                 },
                 modifier = Modifier
